fc29d613f5cb13915dfcf732eda1efbbde78d90d
traore0824 <aliloulayei@gmail.com>
2025-10-13 21:56:12 +0100

    update account app

--- compta/urls.py
+++ compta/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("balance", views.BalanceViews.as_view()),
     path("compta", views.ComptatView.as_view()),
-    path("transaction", views.CreateTransaction.as_view())
+    path("transaction", views.CreateTransaction.as_view()), 
+    
 ]
