b15a0434c6cf02ad6c257c23b5f3286c0d53c9ae
traore0824 <aliloulayei@gmail.com>
2025-10-17 13:06:49 +0100

    update code

--- compta/serializers.py
+++ compta/serializers.py
@@ -32,27 +32,27 @@ class TransactionSerializer(serializers.ModelSerializer):
 
         alerts = []
 
-        if (
-            mobcash_config.can_send_alert
-            and transaction.mobcash_balance <= mobcash_config.minimun_balance_amount
-        ):
-            alerts.append(
-                {
-                    "title": f"Solde critique MobCash: {transaction.mobcash}",
-                    "content": f"Le solde est à {transaction.mobcash_balance} FCFA pour {transaction.mobcash} (seuil: {mobcash_config.minimun_balance_amount})",
-                }
-            )
+        # if (
+        #     mobcash_config.can_send_alert
+        #     and transaction.mobcash_balance <= mobcash_config.minimun_balance_amount
+        # ):
+        #     alerts.append(
+        #         {
+        #             "title": f"Solde critique MobCash: {transaction.mobcash}",
+        #             "content": f"Le solde est à {transaction.mobcash_balance} FCFA pour {transaction.mobcash} (seuil: {mobcash_config.minimun_balance_amount})",
+        #         }
+        #     )
 
-        if (
-            api_config.can_send_alert
-            and transaction.api_balance <= api_config.minimun_balance_amount
-        ):
-            alerts.append(
-                {
-                    "title": f"Solde critique API: {transaction.api}",
-                    "content": f"Le solde est à {transaction.api_balance} FCFA pour {transaction.api} (seuil: {api_config.minimun_balance_amount})",
-                }
-            )
+        # if (
+        #     api_config.can_send_alert
+        #     and transaction.api_balance <= api_config.minimun_balance_amount
+        # ):
+        #     alerts.append(
+        #         {
+        #             "title": f"Solde critique API: {transaction.api}",
+        #             "content": f"Le solde est à {transaction.api_balance} FCFA pour {transaction.api} (seuil: {api_config.minimun_balance_amount})",
+        #         }
+        #     )
 
         for alert in alerts:
             send_telegram_message(
