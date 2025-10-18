b127c9131b0dea7e15ce7da80603c6e804083280
traore0824 <aliloulayei@gmail.com>
2025-10-18 17:42:09 +0100

    update

--- compta/models.py
+++ compta/models.py
@@ -31,7 +31,7 @@ NETWORK_CHOICES = [
 
 
 class Transaction(models.Model):
-    reference = models.CharField(max_length=100)
+    reference = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     mobcash_fee = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     blaffa_fee = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
