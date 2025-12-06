84f3ffb71df6637cc681974557642d1eaea48992
Cascade <cascade@example.com>
2025-12-06 01:16:13 +0100

    update

--- compta/models.py
+++ compta/models.py
@@ -80,7 +80,7 @@ class MobCashApp(models.Model):
 
 class APIBalanceUpdate(models.Model):
     api_transaction = models.ForeignKey(APITransaction, on_delete=models.CASCADE, blank=True, null=True)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
